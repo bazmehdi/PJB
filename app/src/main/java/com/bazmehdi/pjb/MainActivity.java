00195b8bd79eb2f7f18fdd52c547383493dc608e
Basim Mehdi <basim96@hotmail.co.uk>
2018-03-14 14:20:37 +0000

    added main activity functionality and Firebase authentication

--- app/src/main/java/com/bazmehdi/pjb/MainActivity.java
+++ app/src/main/java/com/bazmehdi/pjb/MainActivity.java
@@ -1,8 +1,40 @@
 package com.bazmehdi.pjb;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Baz on 14/03/2018.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private String mDisplayName;
+    private DatabaseReference memberDatabaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Set up the display name and get the Firebase reference
+        setupDisplayName();
+        memberDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+    }
+
+    // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName(){
+
+        SharedPreferences prefs = getSharedPreferences(Register.APP_PREFS, MODE_PRIVATE);
+
+        mDisplayName = prefs.getString(Register.DISPLAY_NAME_KEY, null);
+
+        if (mDisplayName == null) mDisplayName = "Anonymous";
+    }
+
 }
