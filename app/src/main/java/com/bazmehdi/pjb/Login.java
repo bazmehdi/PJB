fb4a3fbfa3d31f9b2d76578ff4a767e5552a7202
Basim Mehdi <basim96@hotmail.co.uk>
2018-03-14 14:00:30 +0000

    added login functionality and Firebase authentication

--- app/src/main/java/com/bazmehdi/pjb/Login.java
+++ app/src/main/java/com/bazmehdi/pjb/Login.java
@@ -1,15 +1,27 @@
 package com.bazmehdi.pjb;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private AutoCompleteTextView memberEmailView;
     private EditText memberPasswordView;
 
@@ -31,5 +43,69 @@ public class Login extends AppCompatActivity {
                 return false;
             }
         });
+
+        // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        // TODO: Call attemptLogin() here
+        attemptLogin();
+
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.bazmehdi.pjb.Register.class);
+        finish();
+        startActivity(intent);
+    }
+
+    // TODO: Complete the attemptLogin() method
+    private void attemptLogin() {
+
+        String email = memberEmailView.getText().toString();
+        String password = memberPasswordView.getText().toString();
+
+        if (email.isEmpty())
+            if (email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                Log.d("PaajeeBakers", "signInWithEmail() onComplete: " + task.isSuccessful());
+
+                if (!task.isSuccessful()) {
+                    Log.d("PaajeeBakers", "Problem signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+    }
+
+    // TODO: Show error on screen with an alert dialog
+    private void showErrorDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+
+
 }
